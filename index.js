180df8bebe7f210c75c5b1c8920b638284d3c64c
weinot <weinotme@gmail.com>
2016-01-15 16:39:50 +0800

    update

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ module.exports.pitch = function(remainingRequest) {
 		throw new Error("file extension should be assigned via 'ext' param");
 	}
 
-	if (new RegExp(ext).test(remainingRequest)) {
+	if (new RegExp(ext + '$', 'ig').test(remainingRequest)) {
 		return [
 			"var cbs = [], \n",
 			"	data;\n",
